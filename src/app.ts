b36a4d864f11879f9e01b2acb2315f7e78461a9e
Nebojsa Lukic <n.lukic@soccerbet.rs>
2024-08-21 13:09:52 +0200

    Added helmet and cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import helmet from "helmet";
+import cors from "cors";
 
 import v1Router from "./api/v1/v1";
 import { loggingHandler } from "./middleware/loggingHandler";
 import { routeNotFound } from "./middleware/routeNotFound";
 
 const app = express();
-// TODO - Add helmet
+
+app.use(cors());
+app.use(helmet());
+
 app.use(bodyParser.json());
 
 app.use("/api/v1", loggingHandler, v1Router);
